f881939f0ad1c3a65fc458ba94274535c82a8ca8
Peter Pivarnik <peter.pivarnik@gmail.com>
2018-09-10 13:55:55 +0200

    Refactored mainView

--- src/main/java/com/psw/cta/client/MainView.java
+++ src/main/java/com/psw/cta/client/MainView.java
@@ -6,6 +6,7 @@ import com.psw.cta.rest.dto.CompleteStats;
 import com.psw.cta.rest.dto.Stats;
 import com.psw.cta.service.CryptoService;
 import com.psw.cta.service.dto.AverageProfit;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -23,92 +24,92 @@ public class MainView extends VerticalLayout {
 
 
     public MainView(@Autowired CryptoService cryptoService) {
-        Label mainLabel = getMainLabel();
-        VerticalLayout allTablesLayout = getAllTablesLayout(cryptoService);
+        Component mainLabel = getMainLabel();
+        Component allTablesLayout = getAllTablesLayout(cryptoService);
         add(mainLabel, allTablesLayout);
     }
 
-    private Label getMainLabel() {
+    private Component getMainLabel() {
         Label messageLabel = new Label();
         messageLabel.setText("Crypto trade analyser");
         messageLabel.setWidth("500px");
         return messageLabel;
     }
 
-    private VerticalLayout getAllTablesLayout(CryptoService cryptoService) {
+    private Component getAllTablesLayout(CryptoService cryptoService) {
         VerticalLayout layout = new VerticalLayout();
         List<CryptoResult> actualCryptos = cryptoService.getActualCryptos();
         CompleteStats stats = cryptoService.getStats();
         AverageProfit averageProfit = cryptoService.getAverageProfit();
-        HorizontalLayout tablesLayout2H = getTablesLayout(actualCryptos,
-                                                          CryptoResult::getPriceToSell2h,
-                                                          CryptoResult::getPriceToSellPercentage2h,
-                                                          stats.getStats2H(),
-                                                          CryptoType.TYPE_2H,
-                                                          averageProfit.getAverage2H());
-        HorizontalLayout tablesLayout5H = getTablesLayout(actualCryptos,
-                                                          CryptoResult::getPriceToSell5h,
-                                                          CryptoResult::getPriceToSellPercentage5h,
-                                                          stats.getStats5H(),
-                                                          CryptoType.TYPE_5H,
-                                                          averageProfit.getAverage5H());
-        HorizontalLayout tablesLayout10H = getTablesLayout(actualCryptos,
-                                                           CryptoResult::getPriceToSell10h,
-                                                           CryptoResult::getPriceToSellPercentage10h,
-                                                           stats.getStats10H(),
-                                                           CryptoType.TYPE_10H,
-                                                           averageProfit.getAverage10H());
-        HorizontalLayout tablesLayout24H = getTablesLayout(actualCryptos,
-                                                           CryptoResult::getPriceToSell24h,
-                                                           CryptoResult::getPriceToSellPercentage24h,
-                                                           stats.getStats24H(),
-                                                           CryptoType.TYPE_24H,
-                                                           averageProfit.getAverage24H());
+        Component tablesLayout2H = getTablesLayout(actualCryptos,
+                                                   CryptoResult::getPriceToSell2h,
+                                                   CryptoResult::getPriceToSellPercentage2h,
+                                                   stats.getStats2H(),
+                                                   CryptoType.TYPE_2H,
+                                                   averageProfit.getAverage2H());
+        Component tablesLayout5H = getTablesLayout(actualCryptos,
+                                                   CryptoResult::getPriceToSell5h,
+                                                   CryptoResult::getPriceToSellPercentage5h,
+                                                   stats.getStats5H(),
+                                                   CryptoType.TYPE_5H,
+                                                   averageProfit.getAverage5H());
+        Component tablesLayout10H = getTablesLayout(actualCryptos,
+                                                    CryptoResult::getPriceToSell10h,
+                                                    CryptoResult::getPriceToSellPercentage10h,
+                                                    stats.getStats10H(),
+                                                    CryptoType.TYPE_10H,
+                                                    averageProfit.getAverage10H());
+        Component tablesLayout24H = getTablesLayout(actualCryptos,
+                                                    CryptoResult::getPriceToSell24h,
+                                                    CryptoResult::getPriceToSellPercentage24h,
+                                                    stats.getStats24H(),
+                                                    CryptoType.TYPE_24H,
+                                                    averageProfit.getAverage24H());
         layout.add(tablesLayout2H, tablesLayout5H, tablesLayout10H, tablesLayout24H);
         return layout;
     }
 
-    private HorizontalLayout getTablesLayout(List<CryptoResult> actualCryptos,
-                                             Function<CryptoResult, BigDecimal> priceToSellFunction,
-                                             Function<CryptoResult, BigDecimal> priceToSellPercFunction,
-                                             Stats stats,
-                                             CryptoType type,
-                                             BigDecimal average) {
+    private Component getTablesLayout(List<CryptoResult> actualCryptos,
+                                      Function<CryptoResult, BigDecimal> priceToSellFunction,
+                                      Function<CryptoResult, BigDecimal> priceToSellPercFunction,
+                                      Stats stats,
+                                      CryptoType type,
+                                      BigDecimal average) {
         HorizontalLayout layout = new HorizontalLayout();
-        VerticalLayout actualCryptoLayout = getActualCryptoLayout(actualCryptos,
-                                                                  priceToSellFunction,
-                                                                  priceToSellPercFunction,
-                                                                  type);
-        VerticalLayout statisticsLayout = getStatisticsLayout(stats, average);
+        Component actualCryptoLayout = getActualCryptoLayout(actualCryptos,
+                                                             priceToSellFunction,
+                                                             priceToSellPercFunction,
+                                                             type);
+        Component statisticsLayout = getStatisticsLayout(stats, average);
         layout.add(actualCryptoLayout, statisticsLayout);
         return layout;
     }
 
-    private VerticalLayout getActualCryptoLayout(List<CryptoResult> actualCryptos,
-                                                 Function<CryptoResult, BigDecimal> priceToSellFunction,
-                                                 Function<CryptoResult, BigDecimal> priceToSellPercFunction,
-                                                 CryptoType type) {
+    private Component getActualCryptoLayout(List<CryptoResult> actualCryptos,
+                                            Function<CryptoResult, BigDecimal> priceToSellFunction,
+                                            Function<CryptoResult, BigDecimal> priceToSellPercFunction,
+                                            CryptoType type) {
         VerticalLayout layout = new VerticalLayout();
-        Label actualCryptoLabel = getActualCryptoLabel();
-        Grid<CryptoResult> cryptoResultGrid = getCryptoJsonGrid(actualCryptos,
-                                                                priceToSellFunction,
-                                                                priceToSellPercFunction,
-                                                                type);
+        Component actualCryptoLabel = getActualCryptoLabel();
+        Component cryptoResultGrid = getCryptoJsonGrid(actualCryptos,
+                                                       priceToSellFunction,
+                                                       priceToSellPercFunction,
+                                                       type);
         layout.add(actualCryptoLabel, cryptoResultGrid);
         return layout;
     }
 
-    private Label getActualCryptoLabel() {
+    private Component getActualCryptoLabel() {
         Label messageLabel = new Label();
         messageLabel.setText("Actual cryptos");
         messageLabel.setWidth("500px");
         return messageLabel;
     }
 
-    private Grid<CryptoResult> getCryptoJsonGrid(List<CryptoResult> actualCryptos,
-                                                 Function<CryptoResult, BigDecimal> priceToSellFunction,
-                                                 Function<CryptoResult, BigDecimal> priceToSellPercFunction,
-                                                 CryptoType type) {
+    private Component getCryptoJsonGrid(List<CryptoResult> actualCryptos,
+                                        Function<CryptoResult, BigDecimal> priceToSellFunction,
+                                        Function<CryptoResult, BigDecimal> priceToSellPercFunction,
+                                        CryptoType type) {
         Grid<CryptoResult> grid = new Grid<>();
         List<CryptoResult> filteredCryptos = actualCryptos.stream()
                 .filter(cryptoResult -> cryptoResult.getCryptoType().equals(type))
@@ -123,33 +124,54 @@ public class MainView extends VerticalLayout {
         return grid;
     }
 
-    private VerticalLayout getStatisticsLayout(Stats stats, BigDecimal average) {
+    private Component getStatisticsLayout(Stats stats, BigDecimal average) {
         VerticalLayout layout = new VerticalLayout();
-        Label statisticsLabel = getStatisticsLabel();
-        Grid<Stats> statisticsGrid = getStatisticsGrid(stats);
-        Label averagePercentLabel = getAveragePercentLabel();
-        Label averagePercentValue = getAveragePercentValue(average);
+        Component statisticsLabel = getStatisticsLabel();
+        Component statisticsGrid = getStatisticsDetailLayout(stats);
+        Component averagePercentLabel = getAveragePercentLabel();
+        Component averagePercentValue = getAveragePercentValue(average);
         layout.add(statisticsLabel, statisticsGrid, averagePercentLabel, averagePercentValue);
         return layout;
     }
 
-    private Label getStatisticsLabel() {
+    private Component getStatisticsLabel() {
         Label label = new Label();
         label.setText("Statistics");
         label.setWidth("500px");
         return label;
     }
 
-    private Grid<Stats> getStatisticsGrid(Stats stats) {
-        Grid<Stats> grid = new Grid<>();
-        grid.setItems(stats);
-        grid.addColumn(Stats::getOneDayStats).setHeader("One day");
-        grid.addColumn(Stats::getOneWeekStats).setHeader("One week");
-        grid.addColumn(Stats::getOneMonthStats).setHeader("One month");
-        return grid;
+    private Component getStatisticsDetailLayout(Stats stats) {
+        HorizontalLayout layout = new HorizontalLayout();
+        Component oneMonthStatsLayout = createStatsLayout("One day", stats.getOneDayStats());
+        Component oneWeekStatsLayout = createStatsLayout("One week", stats.getOneWeekStats());
+        Component oneDayStatsLayout = createStatsLayout("One month", stats.getOneMonthStats());
+        layout.add(oneDayStatsLayout, oneWeekStatsLayout, oneMonthStatsLayout);
+        return layout;
+
     }
 
-    private Label getAveragePercentLabel() {
+    private Component createStatsLayout(String labelName, double stats) {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        Component statLabel = createStatLabel(labelName);
+        Component statlabelvalue = createStatLabelValue(stats);
+        verticalLayout.add(statLabel, statlabelvalue);
+        return verticalLayout;
+    }
+
+    private Component createStatLabelValue(double stats) {
+        Label label = new Label();
+        label.setText(String.valueOf(stats));
+        return label;
+    }
+
+    private Component createStatLabel(String labelName) {
+        Label label = new Label();
+        label.setText(labelName);
+        return label;
+    }
+
+    private Component getAveragePercentLabel() {
         Label label = new Label();
         label.setText("Percentual profit");
         label.setWidth("500px");
@@ -157,7 +179,7 @@ public class MainView extends VerticalLayout {
 
     }
 
-    private Label getAveragePercentValue(BigDecimal average) {
+    private Component getAveragePercentValue(BigDecimal average) {
         Label label = new Label();
         label.setText(average.toString());
         label.setWidth("500px");

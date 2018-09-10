adec03e8d87a0c3178fc72a52843a6e56249efb3
Peter Pivarnik <peter.pivarnik@gmail.com>
2018-09-10 14:38:47 +0200

    Refactored mainView

--- src/main/java/com/psw/cta/client/MainView.java
+++ src/main/java/com/psw/cta/client/MainView.java
@@ -11,6 +11,7 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,22 +23,13 @@ import java.util.stream.Collectors;
 @Route
 public class MainView extends VerticalLayout {
 
-
     public MainView(@Autowired CryptoService cryptoService) {
-        Component mainLabel = getMainLabel();
+        Component mainLabel = getLabel("Crypto trade analyser");
         Component allTablesLayout = getAllTablesLayout(cryptoService);
         add(mainLabel, allTablesLayout);
     }
 
-    private Component getMainLabel() {
-        Label messageLabel = new Label();
-        messageLabel.setText("Crypto trade analyser");
-        messageLabel.setWidth("500px");
-        return messageLabel;
-    }
-
     private Component getAllTablesLayout(CryptoService cryptoService) {
-        VerticalLayout layout = new VerticalLayout();
         List<CryptoResult> actualCryptos = cryptoService.getActualCryptos();
         CompleteStats stats = cryptoService.getStats();
         AverageProfit averageProfit = cryptoService.getAverageProfit();
@@ -65,8 +57,7 @@ public class MainView extends VerticalLayout {
                                                     stats.getStats24H(),
                                                     CryptoType.TYPE_24H,
                                                     averageProfit.getAverage24H());
-        layout.add(tablesLayout2H, tablesLayout5H, tablesLayout10H, tablesLayout24H);
-        return layout;
+        return new VerticalLayout(tablesLayout2H, tablesLayout5H, tablesLayout10H, tablesLayout24H);
     }
 
     private Component getTablesLayout(List<CryptoResult> actualCryptos,
@@ -75,35 +66,24 @@ public class MainView extends VerticalLayout {
                                       Stats stats,
                                       CryptoType type,
                                       BigDecimal average) {
-        HorizontalLayout layout = new HorizontalLayout();
         Component actualCryptoLayout = getActualCryptoLayout(actualCryptos,
                                                              priceToSellFunction,
                                                              priceToSellPercFunction,
                                                              type);
         Component statisticsLayout = getStatisticsLayout(stats, average);
-        layout.add(actualCryptoLayout, statisticsLayout);
-        return layout;
+        return new HorizontalLayout(actualCryptoLayout, statisticsLayout);
     }
 
     private Component getActualCryptoLayout(List<CryptoResult> actualCryptos,
                                             Function<CryptoResult, BigDecimal> priceToSellFunction,
                                             Function<CryptoResult, BigDecimal> priceToSellPercFunction,
                                             CryptoType type) {
-        VerticalLayout layout = new VerticalLayout();
-        Component actualCryptoLabel = getActualCryptoLabel();
+        Component actualCryptoLabel = getLabel("Actual cryptos");
         Component cryptoResultGrid = getCryptoJsonGrid(actualCryptos,
                                                        priceToSellFunction,
                                                        priceToSellPercFunction,
                                                        type);
-        layout.add(actualCryptoLabel, cryptoResultGrid);
-        return layout;
-    }
-
-    private Component getActualCryptoLabel() {
-        Label messageLabel = new Label();
-        messageLabel.setText("Actual cryptos");
-        messageLabel.setWidth("500px");
-        return messageLabel;
+        return new VerticalLayout(actualCryptoLabel, cryptoResultGrid);
     }
 
     private Component getCryptoJsonGrid(List<CryptoResult> actualCryptos,
@@ -125,64 +105,41 @@ public class MainView extends VerticalLayout {
     }
 
     private Component getStatisticsLayout(Stats stats, BigDecimal average) {
-        VerticalLayout layout = new VerticalLayout();
-        Component statisticsLabel = getStatisticsLabel();
-        Component statisticsGrid = getStatisticsDetailLayout(stats);
-        Component averagePercentLabel = getAveragePercentLabel();
-        Component averagePercentValue = getAveragePercentValue(average);
-        layout.add(statisticsLabel, statisticsGrid, averagePercentLabel, averagePercentValue);
-        return layout;
+        Component statisticsLabel = getLabel("Statistics");
+        Component statisticsDetailLayout = getStatisticsDetailLayout(stats);
+        Component statisticsProfitLayout = getStatisticsProfitLayout(average.toString());
+        return new VerticalLayout(statisticsLabel, statisticsDetailLayout, statisticsProfitLayout);
     }
 
-    private Component getStatisticsLabel() {
-        Label label = new Label();
-        label.setText("Statistics");
-        label.setWidth("500px");
-        return label;
+    private Component getLabel(String text) {
+        Label messageLabel = new Label();
+        messageLabel.setText(text);
+        messageLabel.setWidth("500px");
+        return messageLabel;
     }
 
     private Component getStatisticsDetailLayout(Stats stats) {
-        HorizontalLayout layout = new HorizontalLayout();
         Component oneMonthStatsLayout = createStatsLayout("One day", stats.getOneDayStats());
         Component oneWeekStatsLayout = createStatsLayout("One week", stats.getOneWeekStats());
         Component oneDayStatsLayout = createStatsLayout("One month", stats.getOneMonthStats());
-        layout.add(oneDayStatsLayout, oneWeekStatsLayout, oneMonthStatsLayout);
-        return layout;
+        return new HorizontalLayout(oneDayStatsLayout, oneWeekStatsLayout, oneMonthStatsLayout);
+    }
 
+    private Component getStatisticsProfitLayout(String average) {
+        Component averagePercentLabel = createTextField("Percentual profit", average.toString());
+        return new HorizontalLayout(averagePercentLabel);
     }
 
     private Component createStatsLayout(String labelName, double stats) {
-        VerticalLayout verticalLayout = new VerticalLayout();
-        Component statLabel = createStatLabel(labelName);
-        Component statlabelvalue = createStatLabelValue(stats);
-        verticalLayout.add(statLabel, statlabelvalue);
-        return verticalLayout;
+        Component textField = createTextField(labelName, String.valueOf(stats));
+        return new VerticalLayout(textField);
     }
 
-    private Component createStatLabelValue(double stats) {
-        Label label = new Label();
-        label.setText(String.valueOf(stats));
-        return label;
-    }
-
-    private Component createStatLabel(String labelName) {
-        Label label = new Label();
-        label.setText(labelName);
-        return label;
-    }
-
-    private Component getAveragePercentLabel() {
-        Label label = new Label();
-        label.setText("Percentual profit");
-        label.setWidth("500px");
-        return label;
-
-    }
-
-    private Component getAveragePercentValue(BigDecimal average) {
-        Label label = new Label();
-        label.setText(average.toString());
-        label.setWidth("500px");
-        return label;
+    private Component createTextField(String label, String value) {
+        TextField textField = new TextField();
+        textField.setLabel(String.valueOf(label));
+        textField.setValue(value);
+        textField.setReadOnly(true);
+        return textField;
     }
 }

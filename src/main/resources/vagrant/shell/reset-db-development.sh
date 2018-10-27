756dfb93dc30749dc56d9679e98644a560774e27
Peter Pivarnik <p.pivarnik@danubesoft.com>
2018-10-27 14:27:19 +0200

    Refactored to more modules

--- src/main/resources/vagrant/shell/reset-db-development.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo systemctl restart postgresql-9.4
-sudo su postgres --command="psql < /vagrant/db/reset-development.sql"
-exit $?
-
--- src/main/resources/vagrant/vm/tools/adminer/README.txt
+++
@@ -1,1 +0,0 @@
-When updating, rename adminer-X.Y.php to index.php and overwrite current file.

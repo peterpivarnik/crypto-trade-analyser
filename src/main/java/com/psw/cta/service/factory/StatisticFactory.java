bd1c5791df5db8d3acc6064110ab9dc47012e34b
Peter Pivarnik <p.pivarnik@danubesoft.com>
2019-01-08 22:13:26 +0100

    Added Date to statistics

--- src/main/java/com/psw/cta/service/factory/StatisticFactory.java
+++ src/main/java/com/psw/cta/service/factory/StatisticFactory.java
@@ -4,6 +4,9 @@ import com.psw.cta.entity.Statistic;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Component
 public class StatisticFactory {
@@ -11,6 +14,8 @@ public class StatisticFactory {
     public Statistic create(Long createdAt, Long all, Long valid, BigDecimal probability) {
         Statistic statistic = new Statistic();
         statistic.setCreatedAt(createdAt);
+        statistic.setCreatedAtDate(LocalDateTime.ofInstant(Instant.ofEpochMilli(createdAt),
+                                                           ZoneId.of("Europe/Vienna")));
         statistic.setAll(all);
         statistic.setValid(valid);
         statistic.setProbability1h(probability);

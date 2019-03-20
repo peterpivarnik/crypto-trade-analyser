870fb7fd8ffe4ea3357a8fe6562a1c44989977c1
Peter Pivarnik <p.pivarnik@danubesoft.com>
2019-03-20 21:30:01 +0100

    Added sending mail

--- src/main/java/com/psw/cta/client/ContactLayout.java
+++ src/main/java/com/psw/cta/client/ContactLayout.java
@@ -1,19 +1,17 @@
 package com.psw.cta.client;
 
 import com.psw.cta.mail.CryptoMailSender;
-import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.mail.MessagingException;
 
 import static com.psw.cta.client.ClientUtils.createButton;
 import static com.psw.cta.client.ClientUtils.createTextField;
@@ -21,26 +19,51 @@ import static com.psw.cta.client.ClientUtils.createTextField;
 @SpringComponent
 public class ContactLayout {
 
-    @Autowired
     private CryptoMailSender cryptoMailSender;
 
-    public VerticalLayout getLayout() {
+    public ContactLayout(CryptoMailSender cryptoMailSender) {
+        this.cryptoMailSender = cryptoMailSender;
+    }
+
+    VerticalLayout getLayout() {
+        Label contactLabel = prepareContactLabel();
+        TextField contactMail = prepareContactMail();
+        TextArea contactQuestion = prepareContactQuestion();
+        Button submitButton = prepareSubmitButton(contactMail, contactQuestion);
+        return new VerticalLayout(contactLabel, contactMail, contactQuestion, submitButton);
+    }
+
+    private Label prepareContactLabel() {
         Label contactLabel = ClientUtils.getLabel("If you have any question you can contact us here: ");
         contactLabel.setWidth("400px");
         Style style = contactLabel.getStyle();
         style.set("font-weight", "bold");
-        TextField contactMail = createTextField("Your email adress: ");
-        contactMail.setHeight("150px");
-        TextArea contactQuestion = new TextArea("Your question: ");
-        contactQuestion.setSizeFull();
-        Button submitButton = createButton("Submit");
-        submitButton.addClickListener(event -> onEvent(event, contactMail, contactQuestion));
-        return new VerticalLayout(contactLabel, contactMail, contactQuestion, submitButton);
+        return contactLabel;
     }
 
-    private void onEvent(ClickEvent<Button> event,
-                         TextField contactMail,
-                         TextArea contactQuestion) {
+    private TextField prepareContactMail() {
+        TextField contactMail = createTextField("Your email adress: ");
+        contactMail.setHeight("150px");
+        Binder<TextField> binder = new Binder<>();
+        binder.forField(contactMail)
+                .withValidator(new EmailValidator("Not valid email"))
+                .bind(TextField::getValue, TextField::setValue);
+        return contactMail;
+    }
+
+    private TextArea prepareContactQuestion() {
+        TextArea contactQuestion = new TextArea("Your question: ");
+        contactQuestion.setSizeFull();
+        return contactQuestion;
+    }
+
+    private Button prepareSubmitButton(TextField contactMail, TextArea contactQuestion) {
+        Button submitButton = createButton("Submit");
+        submitButton.addClickListener(event -> onEvent(contactMail, contactQuestion));
+        return submitButton;
+    }
+
+    private void onEvent(TextField contactMail, TextArea contactQuestion) {
         if (StringUtils.isEmpty(contactMail.getValue())) {
             showNotification("Please enter your email.");
             return;
@@ -49,11 +72,7 @@ public class ContactLayout {
             showNotification("Please enter your question.");
             return;
         }
-        try {
-            cryptoMailSender.send();
-        } catch (MessagingException e) {
-            throw new RuntimeException("Exception during sending mail", e);
-        }
+        cryptoMailSender.send(contactMail.getValue(), contactQuestion.getValue());
         showNotification("Your question had been send.");
         contactMail.setValue("");
         contactQuestion.setValue("");
--- src/main/java/com/psw/cta/mail/CryptoMailSender.java
+++ src/main/java/com/psw/cta/mail/CryptoMailSender.java
@@ -9,47 +9,65 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.time.Instant;
 import java.util.Properties;
 
 @Component
 public class CryptoMailSender {
 
-    public void send() throws MessagingException {
+    public void send(String mail, String question) {
         String to = "cryptotradeanalyser@gmail.com";
-        String subject = "subject";
-        String msg ="email text....";
-        final String from ="cryptotradeanalyser@gmail.com";
-        final  String password ="cta#123456";
+        String subject = "CTA Question nr: " + Instant.now().toEpochMilli();
+        final String from = "cryptotradeanalyser@gmail.com";
+        final String password = "cta#123456";
 
+        Properties properties = prepareProperties();
+        Session session = Session.getDefaultInstance(
+                properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(from, password);
+                    }
+                });
+
+        try {
+            Transport transport = session.getTransport();
+            InternetAddress addressFrom = new InternetAddress(from);
+            MimeMessage message = prepareMimeMessage(mail, question, to, subject, session, addressFrom);
+            transport.connect();
+            Transport.send(message);
+            transport.close();
+        } catch (MessagingException e) {
+            throw new RuntimeException("Problem during sending mail", e);
+        }
+    }
+
+    private MimeMessage prepareMimeMessage(String mail,
+                                           String question,
+                                           String to,
+                                           String subject,
+                                           Session session,
+                                           InternetAddress addressFrom) throws MessagingException {
+        MimeMessage message = new MimeMessage(session);
+        message.setSender(addressFrom);
+        message.setSubject(subject);
+        message.setContent(question, "text/plain");
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(mail));
+        return message;
+    }
+
+    private Properties prepareProperties() {
         Properties props = new Properties();
-        props.setProperty("mail.transport.protocol", "smtp");
-        props.setProperty("mail.host", "smtp.gmail.com");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.host", "smtp.gmail.com");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.port", "465");
         props.put("mail.debug", "true");
         props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
-        Session session = Session.getDefaultInstance(props,
-                                                     new javax.mail.Authenticator() {
-                                                         protected PasswordAuthentication getPasswordAuthentication() {
-                                                             return new PasswordAuthentication(from,password);
-                                                         }
-                                                     });
-
-        //session.setDebug(true);
-        Transport transport = session.getTransport();
-        InternetAddress addressFrom = new InternetAddress(from);
-
-        MimeMessage message = new MimeMessage(session);
-        message.setSender(addressFrom);
-        message.setSubject(subject);
-        message.setContent(msg, "text/plain");
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-        transport.connect();
-        Transport.send(message);
-        transport.close();
+        return props;
     }
 }
 

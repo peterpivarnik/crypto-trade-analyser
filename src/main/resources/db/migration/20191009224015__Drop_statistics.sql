153b960113828d6446d2129216378359c64b6fbc
Peter Pivarnik <p.pivarnik@danubesoft.com>
2019-10-09 22:45:17 +0200

    Removed statistics

---
+++ src/main/resources/db/migration/20191009224015__Drop_statistics.sql
@@ -0,0 +1,6 @@
+DROP TABLE statistic;
+DROP TABLE statistic_2day;
+DROP TABLE statistic_week;
+DROP SEQUENCE seq_statistic_id;
+DROP SEQUENCE seq_statistic_2day_id;
+DROP SEQUENCE seq_statistic_week_id;

34e808c30e3db14d0678eab9e1b578efb9b0fbfe
peterpivarnik <peter.pivarnik.ext@porscheinformatik.com>
2022-04-10 21:29:55 +0200

    Added javadoc

--- src/main/java/com/psw/cta/utils/CommonUtils.java
+++ src/main/java/com/psw/cta/utils/CommonUtils.java
@@ -1,14 +1,5 @@
 package com.psw.cta.utils;
 
-import static com.binance.api.client.domain.general.FilterType.LOT_SIZE;
-import static com.binance.api.client.domain.general.FilterType.PRICE_FILTER;
-import static com.psw.cta.utils.Constants.HUNDRED_PERCENT;
-import static com.psw.cta.utils.LeastSquares.getSlope;
-import static java.math.RoundingMode.CEILING;
-import static java.math.RoundingMode.UP;
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toMap;
-
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.binance.api.client.domain.account.Order;
 import com.binance.api.client.domain.general.FilterType;
@@ -18,12 +9,8 @@ import com.binance.api.client.domain.market.Candlestick;
 import com.binance.api.client.domain.market.OrderBook;
 import com.binance.api.client.domain.market.OrderBookEntry;
 import com.psw.cta.exception.CryptoTraderException;
-import com.psw.cta.service.BinanceApiService;
-import com.psw.cta.service.BnbService;
-import com.psw.cta.service.DiversifyService;
-import com.psw.cta.service.InitialTradingService;
-import com.psw.cta.service.RepeatTradingService;
-import com.psw.cta.service.TradingService;
+import com.psw.cta.service.*;
+
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Comparator;
@@ -32,6 +19,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static com.binance.api.client.domain.general.FilterType.LOT_SIZE;
+import static com.binance.api.client.domain.general.FilterType.PRICE_FILTER;
+import static com.psw.cta.utils.Constants.HUNDRED_PERCENT;
+import static com.psw.cta.utils.LeastSquares.getSlope;
+import static java.math.RoundingMode.CEILING;
+import static java.math.RoundingMode.UP;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toMap;
 
 public class CommonUtils {
 
@@ -49,11 +45,17 @@ public class CommonUtils {
         Function<Order, BigDecimal> btcAmountFunction = order -> (getQuantity(order)).multiply(new BigDecimal(order.getPrice()));
         Function<Order, BigDecimal> timeFunction = order -> new BigDecimal(order.getTime());
         return comparing(quantityFunction)
-            .reversed()
-            .thenComparing(comparing(btcAmountFunction).reversed())
-            .thenComparing(timeFunction);
+                .reversed()
+                .thenComparing(comparing(btcAmountFunction).reversed())
+                .thenComparing(timeFunction);
     }
 
+    /**
+     * Sleep program for provided amount of miliseconds
+     *
+     * @param millis Miliseconds to sleep
+     * @param logger Logger to log the exception
+     */
     public static void sleep(int millis, LambdaLogger logger) {
         try {
             Thread.sleep(millis);
@@ -64,12 +66,12 @@ public class CommonUtils {
 
     public static BigDecimal getValueFromFilter(SymbolInfo symbolInfo, FilterType filterType, Function<SymbolFilter, String> symbolFilterFunction) {
         return symbolInfo.getFilters()
-                         .parallelStream()
-                         .filter(filter -> filter.getFilterType().equals(filterType))
-                         .map(symbolFilterFunction)
-                         .map(BigDecimal::new)
-                         .findAny()
-                         .orElseThrow(() -> new CryptoTraderException("Value from filter " + filterType + " not found"));
+                .parallelStream()
+                .filter(filter -> filter.getFilterType().equals(filterType))
+                .map(symbolFilterFunction)
+                .map(BigDecimal::new)
+                .findAny()
+                .orElseThrow(() -> new CryptoTraderException("Value from filter " + filterType + " not found"));
     }
 
     public static BigDecimal roundAmount(SymbolInfo symbolInfo, BigDecimal amount) {
@@ -101,12 +103,12 @@ public class CommonUtils {
 
     public static List<BigDecimal> getAveragePrices(List<Candlestick> threeMonthsCandleStickData) {
         Candlestick maxHighCandlestick = threeMonthsCandleStickData.stream()
-                                                            .max(comparing(candle -> new BigDecimal(candle.getHigh())))
-                                                            .orElseThrow();
+                .max(comparing(candle -> new BigDecimal(candle.getHigh())))
+                .orElseThrow();
         return threeMonthsCandleStickData.parallelStream()
-                                         .filter(candle -> candle.getOpenTime() > maxHighCandlestick.getOpenTime())
-                                         .map(CommonUtils::getAveragePrice)
-                                         .collect(Collectors.toList());
+                .filter(candle -> candle.getOpenTime() > maxHighCandlestick.getOpenTime())
+                .map(CommonUtils::getAveragePrice)
+                .collect(Collectors.toList());
     }
 
     public static BigDecimal getAveragePrice(Candlestick candle) {
@@ -115,20 +117,20 @@ public class CommonUtils {
         BigDecimal high = new BigDecimal(candle.getHigh());
         BigDecimal low = new BigDecimal(candle.getLow());
         return open.add(close)
-                   .add(high)
-                   .add(low)
-                   .divide(new BigDecimal("4"), 8, CEILING);
+                .add(high)
+                .add(low)
+                .divide(new BigDecimal("4"), 8, CEILING);
     }
 
     public static Map<String, BigDecimal> createTotalAmounts(List<Order> openOrders) {
         return openOrders.stream()
-                         .collect(toMap(Order::getSymbol,
-                                        order -> new BigDecimal(order.getPrice()).multiply(getQuantity(order)),
-                                        BigDecimal::add))
-                         .entrySet()
-                         .stream()
-                         .sorted(Map.Entry.comparingByValue())
-                         .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+                .collect(toMap(Order::getSymbol,
+                        order -> new BigDecimal(order.getPrice()).multiply(getQuantity(order)),
+                        BigDecimal::add))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
     public static BigDecimal getQuantity(Order order) {
@@ -143,16 +145,16 @@ public class CommonUtils {
 
     public static BigDecimal getCurrentPrice(OrderBook orderBook) {
         return orderBook.getAsks()
-                        .parallelStream()
-                        .map(OrderBookEntry::getPrice)
-                        .map(BigDecimal::new)
-                        .min(Comparator.naturalOrder())
-                        .orElseThrow(() -> new CryptoTraderException("No price found!"));
+                .parallelStream()
+                .map(OrderBookEntry::getPrice)
+                .map(BigDecimal::new)
+                .min(Comparator.naturalOrder())
+                .orElseThrow(() -> new CryptoTraderException("No price found!"));
     }
 
     public static BigDecimal calculatePricePercentage(BigDecimal lowestPrice, BigDecimal highestPrice) {
         BigDecimal percentage = lowestPrice.multiply(HUNDRED_PERCENT)
-                                           .divide(highestPrice, 8, UP);
+                .divide(highestPrice, 8, UP);
         return HUNDRED_PERCENT.subtract(percentage);
     }
 

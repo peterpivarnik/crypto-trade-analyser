184c4f1de28906e70fa30abc2fa033625afe3ffb
peterpivarnik <peter.pivarnik@gmail.com>
2025-03-27 11:01:34 +0100

    Added secret manger to test

--- src/main/java/com/psw/cta/ServiceHandler.java
+++ src/main/java/com/psw/cta/ServiceHandler.java
@@ -1,11 +1,16 @@
 package com.psw.cta;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.psw.cta.dto.Input;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 /**
  * Main class to be used in AWS lambda.
@@ -23,11 +28,41 @@ public class ServiceHandler implements RequestHandler<Input, Object> {
                                                  variables.get("apiSecret"),
                                                  forbiddenPairs,
                                                  context.getLogger());
+
+
+    getSecret(context.getLogger());
     cryptoTrader.startTrading();
     return "Lambda Function is invoked....";
   }
 
   private List<String> splitForbiddenPairs(String forbiddenPairs) {
     return Arrays.asList(forbiddenPairs.split(","));
+  }
+
+  private void getSecret(LambdaLogger logger) {
+    String secretName = "testSecret";
+    Region region = Region.of("ap-northeast-1");
+
+    // Create a Secrets Manager client
+    SecretsManagerClient client = SecretsManagerClient.builder()
+                                                      .region(region)
+                                                      .build();
+
+    GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                                                                       .secretId(secretName)
+                                                                       .build();
+
+    GetSecretValueResponse getSecretValueResponse;
+
+    try {
+      getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+    } catch (Exception e) {
+      // For a list of exceptions thrown, see
+      // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+      throw e;
+    }
+
+    String secret = getSecretValueResponse.secretString();
+    logger.log("Secret: " + secret);
   }
 }
